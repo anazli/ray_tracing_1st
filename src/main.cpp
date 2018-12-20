fbad48b9516011c927879163211d4297c7fc941a
anazli <andreas221b@gmail.com>
2018-12-20 23:38:25 +0100

    end of chapter 6

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<fstream>
+#include "../include/camera.h"
 #include "../include/ray.h"
 #include "../include/sphere.h"
 #include "../include/hitable.h"
@@ -23,31 +24,32 @@ int main()
 {
     int nx = 800;
     int ny = 600;
+    int ns = 100;
     ofstream out;
     out.open("image.ppm");
 
-    Vec3 lower_left_corner(-2., -1., -1.);
-    Vec3 horizontal(4., 0., 0.);
-    Vec3 vertical(0., 2., 0.);
-    Vec3 origin(0., 0., 0.);
 
     Hitable *list[2];
     list[0] = new Sphere(Vec3(0.,0.,-1), 0.5);
     list[1] = new Sphere(Vec3(0., -100.5, -1.), 100);
     Hitable *world = new Hitable_list(list, 2);
+    Camera cam;
 
     out << "P3\n" << nx << " " << ny << "\n255\n";
     for(int j = ny ; j >= 0 ; --j)
     {
         for(int i = 0 ; i < nx ; ++i)
         {
+            Vec3 col(0.,0.,0.);
+            for(int s = 0 ; s < ns ; ++s)
+            {
+                double u = double(i + drand48())/double(nx);
+                double v = double(j + drand48())/double(ny);
 
-            double u = double(i)/double(nx);
-            double v = double(j)/double(ny);
-
-            Ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            Vec3 col = color(r, world);
-
+                Ray r = cam.get_ray(u,v);
+                col = col + color(r, world);
+            }
+            col = col/ double(ns);
             int ir = int(255.99*col.x());
             int ig = int(255.99*col.y());
             int ib = int(255.99*col.z());
