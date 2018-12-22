3f810aea29162757a949f15b2e99ac01f3e15993
anazli <andreas221b@gmail.com>
2018-12-22 17:01:37 +0100

    end of book

--- include/material.h
+++ include/material.h
@@ -6,9 +6,30 @@ struct hit_record;
 #include "ray.h"
 #include "hitable.h"
 
+double schlick(double cosine, double ref_idx)
+{
+    double r0 = (1. - ref_idx)/(1. + ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1. - r0) * pow( (1. - cosine), 5);
+}
+
 Vec3 reflect(const Vec3& v, const Vec3& n)
 {
     return v - 2. * dot(v,n)*n;
+}
+
+bool refract(const Vec3& v, const Vec3& n, double ni_over_nt, Vec3& refracted)
+{
+    Vec3 uv = getUnitVectorOf(v);
+    double dt = dot(uv, n);
+    double discriminant = 1. - ni_over_nt * ni_over_nt * (1. - dt*dt);
+    if(discriminant > 0)
+    {
+        refracted = ni_over_nt * (uv - n*dt) - n*sqrt(discriminant);
+        return true;
+    }
+    else
+        return false;
 }
 
 class Material {
@@ -59,6 +80,55 @@ class Metal : public Material {
 
         Vec3 albedo;
         double fuzz;
+};
+
+class Dielectric : public Material {
+
+    public:
+
+        Dielectric(double ri) : ref_idx(ri){}
+
+        virtual bool scatter(const Ray& r_in, const hit_record& rec,
+                             Vec3& attenuation, Ray& scattered) const 
+        {
+            Vec3 outward_normal;
+            Vec3 reflected = reflect(r_in.direction(), rec.normal);
+            double ni_over_nt;
+            attenuation = Vec3(1., 1., 1.);
+            Vec3 refracted;
+            double reflect_prob;
+            double cosine;
+            if(dot(r_in.direction(), rec.normal) > 0)
+            {
+                outward_normal = -rec.normal;
+                ni_over_nt = ref_idx;
+                //cosine = ref_idx * dot(r_in.direction(), rec.normal)/
+                                       // r_in.direction().magnitude();
+                cosine = dot(r_in.direction(), rec.normal) / r_in.direction().magnitude();
+                cosine = sqrt(1 - ref_idx*ref_idx*(1-cosine*cosine));
+            }
+            else
+            {
+                outward_normal = rec.normal;
+                ni_over_nt = 1./ref_idx;
+                cosine = -dot(r_in.direction(), rec.normal)/
+                                        r_in.direction().magnitude();
+            }
+
+            if(refract(r_in.direction(), outward_normal, ni_over_nt, refracted))
+                reflect_prob = schlick(cosine, ref_idx);
+            else
+                reflect_prob = 1.;
+
+            if(drand48() < reflect_prob)
+                scattered = Ray(rec.p, reflected);
+            else
+                scattered = Ray(rec.p, refracted);
+
+            return true;
+        }
+
+    double ref_idx;      
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 
 using namespace std;
 
+
+Hitable *random_scene();
+
 Vec3 color(const Ray& r, Hitable *world, int depth)
 {
     hit_record rec;
@@ -34,19 +37,30 @@ int main()
 {
     int nx = 800;
     int ny = 600;
-    int ns = 100;
+    int ns = 10;
     ofstream out;
     out.open("image.ppm");
 
 
-    int Nobj = 4;
+    /*int Nobj = 5;
     Hitable *list[Nobj];
     list[0] = new Sphere(Vec3(0.,0.,-1), 0.5, new Lambertian(Vec3(0.8, 0.3, 0.3)));
     list[1] = new Sphere(Vec3(0., -100.5, -1.), 100, new Lambertian(Vec3(0.8,0.8,0.)));
     list[2] = new Sphere(Vec3(1., 0., -1.), 0.5, new Metal(Vec3(0.8,0.6,0.2), 0.0));
-    list[3] = new Sphere(Vec3(-1., 0., -1.), 0.5, new Metal(Vec3(0.8,0.8,0.8), 0.0));
-    Hitable *world = new Hitable_list(list, Nobj);
-    Camera cam;
+    list[3] = new Sphere(Vec3(-1., 0., -1.), 0.5, new Dielectric(1.5));
+    list[4] = new Sphere(Vec3(-1., 0., -1.), -0.45, new Dielectric(1.5));
+
+    Hitable *world = new Hitable_list(list, Nobj);*/
+
+    Hitable *world = random_scene();
+
+    Vec3 lookfrom(13., 2., 3.);
+    Vec3 lookat(0., 0., 0);
+    double dist_to_focus = 10.;
+    double aperture = 0.1;
+
+    Camera cam(lookfrom, lookat, Vec3(0., 1., 0.), 20., double(nx)/double(ny),
+                                                     aperture, dist_to_focus);
 
     out << "P3\n" << nx << " " << ny << "\n255\n";
     for(int j = ny ; j >= 0 ; --j)
@@ -75,4 +89,46 @@ int main()
     out.close();
 
     return 0;
+}
+
+Hitable *random_scene()
+{
+    int n = 500;
+    Hitable **list = new Hitable*[n+1];
+    list[0] = new Sphere(Vec3(0., -1000., 0.), 1000, new Lambertian(Vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for(int a = -11 ; a < 11 ; a++)
+    {
+        for(int b = -11 ; b < 11 ; b++)
+        {
+            double choose_mat = drand48();
+            Vec3 center(a + 0.9*drand48(), 0.2, b + 0.9*drand48());
+            if((center - Vec3(4., 0.2, 0.)).magnitude() > 0.9)
+            {
+                if(choose_mat < 0.8) //diffuse
+                {
+                    list[i++] = new Sphere(center, 0.2,
+                         new Lambertian(Vec3(drand48()*drand48(),
+                                     drand48()*drand48(), drand48()*drand48())));
+                }
+                else if(choose_mat < 0.95) //metal
+                {
+                    list[i++] = new Sphere(center, 0.2,
+                         new Metal(Vec3(0.5 * (1. + drand48()),
+                                     0.5 * (1. + drand48()), 0.5 * (1. + drand48())), 0.5*drand48()));
+                    
+                }
+                else
+                {
+                    list[i++] = new Sphere(center, 0.2, new Dielectric(1.5));
+                }
+            }
+        }
+    }
+
+    list[i++] = new Sphere(Vec3(0., 1., 0.), 1., new Dielectric(1.5));
+    list[i++] = new Sphere(Vec3(-4., 1., 0.), 1., new Lambertian(Vec3(0.4, 0.2, 0.1)));
+    list[i++] = new Sphere(Vec3(4., 1., 0.), 1., new Metal(Vec3(0.7, 0.6, 0.5), 0.));
+
+    return new Hitable_list(list, i);
 }
